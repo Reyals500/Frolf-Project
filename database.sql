2cfe15aeecb95e64e3e293cf7e6f3b241a800128
Michael Jancik-Kitowski <michael.jancik24@gmail.com>
2024-04-01 16:35:17 -0500

    Working on the database and trying to see what I can make work

--- database.sql
+++ database.sql
@@ -7,3 +7,17 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+CREATE TABLE "course" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (255) UNIQUE NOT NULL,
+    "location" VARCHAR (255) NOT NULL,
+    "par" INTEGER NOT NULL
+);
+
+CREATE TABLE "score" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INTEGER REFERENCES "user"("id"),
+    "course_id" INTEGER REFERENCES "course"("id"),
+    "score" INTEGER NOT NULL,
+    "date_played" DATE NOT NULL
+);
